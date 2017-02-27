03056b8a5a6fbad8e3642f9530eae32c1d5e3e70
Duncan <duncan.a.rowland@gmail.com>
2017-02-27 20:32:26 +0000

    removed defunct dns

--- render.sh
+++ render.sh
@@ -2,7 +2,7 @@
 
 ID=$$
 cat map.html.template | sed s/SPECIES/"$1"/g > tmp/map.$ID.html
-python python-webkit2png/webkit2png/scripts.py http://neoalgorist.org/maprender/tmp/map.$ID.html -x 900 900 -W --output=tmp/map.$ID.png -F javascript
+python python-webkit2png/webkit2png/scripts.py http://localhost/maprender/tmp/map.$ID.html -x 900 900 -W --output=tmp/map.$ID.png -F javascript
 rm tmp/map.$ID.html
 echo $ID
 
